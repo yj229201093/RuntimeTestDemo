0fcf2112e9677e13e6a91f8c1907f8c42beb2210
YangJian <229201093@qq.com>
2016-06-02 18:46:29 +0800

    Initial Commit

---
+++ RuntimeTestDemo/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  RuntimeTestDemo
+//
+//  Created by GongHui_YJ on 16/6/2.
+//  Copyright © 2016年 YangJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ RuntimeTestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RuntimeTestDemo
+//
+//  Created by GongHui_YJ on 16/6/2.
+//  Copyright © 2016年 YangJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
